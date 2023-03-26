846bfc078fe7e987bcb4300764a6552e2d6753cf
Joss White <94143372+jossw24@users.noreply.github.com>
2023-03-26 21:04:07 +0000

    Make some mock tests in virtual environment and created a django app home

--- sensingGang/home/tests.py
+++ sensingGang/home/tests.py
@@ -3,4 +3,5 @@ import unittest
 # Create your tests here.
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(1 + 1, 5)
+
