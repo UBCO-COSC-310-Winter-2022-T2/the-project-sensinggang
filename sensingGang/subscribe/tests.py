b18616393437a13087b6df0d70a85a078b6b0cf6
Mackenzie Kudrenecky <112984152+mkudrenecky@users.noreply.github.com>
2023-03-28 04:10:55 +0000

    feat: testing + publish

--- sensingGang/subscribe/tests.py
+++ sensingGang/subscribe/tests.py
@@ -1,8 +1,10 @@
 import unittest
 from django.test import TestCase
 import paho.mqtt.client as mqtt
+from unittest import TestCase, mock
 
 client = mqtt.Client("test_client")
+
 # Create your tests here.
 class MyTestCase(unittest.TestCase):
     def test_something(self):
@@ -13,13 +15,30 @@ class MyTestCase(unittest.TestCase):
         # Call is_conected() built in function that returns t/f
         self.assertEqual(client.is_connected(), True)
         
-    def test_subscribe(self, mock_subscribe):
+    def test_disconnect(self):
+         self.assertEqual(client.is_connected(), True)
+ 
+    def test_subscribe(self):
         # Define the expected arguments
         topic = 'mytopic'
         qos = 0
-
-        # Call the subscribe function with the expected arguments
+        
+        mock_subscribe = mock.MagicMock()
+        client.subscribe = mock_subscribe
+        
+         # Call the subscribe function with the expected arguments
         client.subscribe(topic, qos)
 
         # Assert that the MQTT client's subscribe method was called with the expected arguments
         mock_subscribe.assert_called_once_with(topic, qos)
+        
+    def test_publish(self):
+        received_message = 1
+        expected_message = 2
+        self.assertEqual(received_message, expected_message)
+        
+    def test_unsubscribe(self):
+        topic = "test_topic"
+        client.subscribe(topic)
+        client.is_subscribed("topic")
+        self.assertEqual(client.is_subscribed(topic), False)
