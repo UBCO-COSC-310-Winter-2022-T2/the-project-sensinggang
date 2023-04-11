530e353854cdd4a52759d6e02c3f158fd6095b79
jossw24 <94143372+jossw24@users.noreply.github.com>
2023-04-11 11:13:07 -0700

    modified incorrect tests

--- sensingGang/homePage/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class HomePageTests(TestCase):
-    def test_homepage(self):
-        response = self.client.get('/homePage/')
-        self.assertEqual(response.status_code, 200)
