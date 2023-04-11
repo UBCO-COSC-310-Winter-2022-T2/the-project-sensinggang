c04d71d83cc8b3301ea93b4d9a71dd180632f6b8
aryanagn <aryanbhat87@gmail.com>
2023-04-11 10:46:53 -0700

    chartjs graph representation

--- sensingGang/graphs/graphsDisplay/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('graphsDisplay/', views.graphsDisplay, name='graphsDisplay'),
-    path('sensor_data/', views.sensor_data, name='sensor_data'),
-]
