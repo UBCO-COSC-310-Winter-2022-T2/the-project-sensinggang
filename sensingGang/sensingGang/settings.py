5baea952c0a615f13f0e1e5a3abbe5c987914a09
samuelspencer164 <97272301+samuelspencer164@users.noreply.github.com>
2023-04-11 11:24:56 -0700

    Update settings.py
    fixing a bug with the static url settings

--- sensingGang/sensingGang/settings.py
+++ sensingGang/sensingGang/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [STATIC_URL]
+STATICFILES_DIRS = ["static/"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
