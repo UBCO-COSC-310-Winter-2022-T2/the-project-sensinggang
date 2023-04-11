b50f1f349942796ee5bef2102411d19cd60bee59
samuelspencer164 <samuelspencer164@gmail.com>
2023-04-11 11:11:05 -0700

    changed homePage connection test to reflect path changes

--- sensingGang/homePage/tests.py
+++ sensingGang/homePage/tests.py
@@ -2,5 +2,5 @@ from django.test import TestCase
 
 class HomePageTests(TestCase):
     def test_homepage(self):
-        response = self.client.get('/homePage/')
+        response = self.client.get('')
         self.assertEqual(response.status_code, 200)
