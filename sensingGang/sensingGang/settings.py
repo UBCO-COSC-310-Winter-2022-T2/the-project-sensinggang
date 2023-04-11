1073e465cd70be1629604227483dccbafba30b1b
jossw24 <94143372+jossw24@users.noreply.github.com>
2023-04-11 11:29:48 -0700

    Setting.py modifications

--- sensingGang/sensingGang/settings.py
+++ sensingGang/sensingGang/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [STATIC_URL]
+# STATIC_URL = "static/"
+# STATICFILES_DIRS = [STATIC_URL]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
