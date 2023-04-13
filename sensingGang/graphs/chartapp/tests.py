891cd89e1e86ab359104731af13b64fd01c750ae
aryanagn <aryanbhat87@gmail.com>
2023-04-13 14:25:59 -0700

    altered tests

--- sensingGang/graphs/chartapp/tests.py
+++ sensingGang/graphs/chartapp/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from chartapp.forms import ProductForm
 
-# Create your tests here.
+
+class ChartJSTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('index')
+
+
+    def test_chart_exists(self):
+        response = self.client.get(reverse('index'))
+        self.assertContains(response, '<canvas id="myChart"')
+        
+    def test_server_connection(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_index_view_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'chartapp/index.html')
+        self.assertTrue('products' in response.context)
+        self.assertTrue('form' in response.context)
+        self.assertIsInstance(response.context['form'], ProductForm)
+        
