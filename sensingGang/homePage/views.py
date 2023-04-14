6e90928e7f1867ca39e5e0b8067a7972d58105bc
Mackenzie Kudrenecky <112984152+mkudrenecky@users.noreply.github.com>
2023-04-14 19:59:02 +0000

    feat: live update button

--- sensingGang/homePage/views.py
+++ sensingGang/homePage/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from subscribe.views import show_data
+from subscribe.views import show_data, generate_data
 
 def homePage(request):
     # set context with call to show_data function from subscribe.views
     context = show_data(request)
     return render(request, "homePage/homePageTemplate.html", context)
+
+def button_view(request):
+    # generate "live" data
+    generate_data()
+    
+    # set context with call to show_data function from subscribe.views
+    context = show_data(request)
+    return render(request, 'homePage/homePageTemplate.html', context)
